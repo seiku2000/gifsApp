61cd61277e4ee72877c34e8f03cf2e4a8b944a30
seiku2000 <eslumter@gmail.com>
2026-09-03 21:31:26 -0700

    componets

---
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list-item',
+  imports: [],
+  templateUrl: './gif-list-item.html',
+})
+export class GifListItem {}
--- src/app/gifs/components/side-menu/side-menu.ts
+++ src/app/gifs/components/side-menu/side-menu.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { SideMenuHeader } from "../side-menu-header/side-menu-header";
-import { SideMenuOptions } from "../side-menu-options/side-menu-options";
+import { SideMenuHeader } from "./side-menu-header/side-menu-header";
+import { SideMenuOptions } from "./side-menu-options/side-menu-options";
 
 @Component({
   selector: 'app-side-menu',
