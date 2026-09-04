2eeae3c1fbd26aba1d8a3caea043eb1b9115b25f
seiku2000 <eslumter@gmail.com>
2026-09-04 16:28:05 -0700

    components

--- src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-gif-list-item',
   imports: [],
   templateUrl: './gif-list-item.html',
 })
-export class GifListItem {}
+export class GifListItem {
+
+  url = input.required<string>();
+
+}
