d9cd8161e8d27983e5194ca29a19663a3bdd2873
seiku2000 <eslumter@gmail.com>
2026-09-06 21:14:53 -0700

    observables

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { GifList } from "../../components/gif-list/gif-list";
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 @Component({
   selector: 'app-search-page',
   imports: [MatFormFieldModule, MatInputModule, MatIconModule, GifList],
@@ -11,8 +13,15 @@ import { GifList } from "../../components/gif-list/gif-list";
 export default class SearchPage {
 
   // public serchGifs = signal([])
+  public gifs = signal<Gif[]>([]);
+
+  public gifSearchService = inject(GifsService);
 
   public onSearch(query: string) {
-    console.log(query);
+    // this.gifSearchService.searchGifs(query);
+    //console.log(query);
+    this.gifSearchService.searchGifs(query).subscribe(rest => {
+      console.log(rest.data);
+    })
   }
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment'; //variables de entorno
 import type { Giphy } from '../interfaces/giphy.interface'; // tipado de la respuesta
 import { Gif } from '../interfaces/gif.interface';
 import { GiphyItemMapper } from '../mapper/gif.mapper';//este para traformar la data que solo necesitemo del api sin necesidad estar tomando todo
+import { tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
@@ -41,8 +42,33 @@ export class GifsService {
             this.trendingGifs.set(gifs);//establecemos los gifs en el signal
             this.trendingGifsLoading.set(false);
             console.log(gifs);
-        })
+        });
 
 
+    }
+    // retornamos un observale un observable es como un flujo de datos que se puede suscribir a el.
+    searchGifs(query: string) {
+        return this.http.get<Giphy>(`${environment.giphyUrl}/gifs/search`, {
+            params: {
+                api_key: environment.giphyKey,
+                limit: 20,
+                q: query
+            }
+            //pipe me permite encadenar operadores y transformar los datos que se reciben , ejemplo si quiero que me retorne solo una propiedad del objeto que me devuelve el api
+        }).pipe(
+            //tab sirve para ejecutar efectos secundarios. por ejemplo llamar a otro servicio o hacer console.log
+            tap(resp => console.log({ tap: resp })),
+            tap(resp => console.log({ tap2: resp })),
+            tap(resp => console.log({ tap3: resp })),
+
+
+        )
+
+
+        /*.subscribe((resp: Giphy) => {
+            const gifsSearch = GiphyItemMapper.mapGiphyItemsToGifAray(resp.data);
+            console.log(gifsSearch);
+            // console.log(resp.data);
+        });*/
     }
 }
